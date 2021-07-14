20120d62e23f4fad601c1fe0f475d5c90171102e
vera <vera@vinova.com.sg>
2021-07-14 22:46:11 +0700

    init add flat list

--- components/CategoryListItem/index.tsx
+++ components/CategoryListItem/index.tsx
@@ -3,7 +3,11 @@ import styles from './styles';
 import { Text, View } from '../Themed';
 import {Image} from 'react-native';
 
-export default function CategoryListItem() {
+interface Props {
+  category: any
+}
+
+export default function CategoryListItem(props: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Tao la nhat ne</Text>
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,29 +1,49 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import CategoryListItem from '../components/CategoryListItem';
-import { ScrollView } from 'react-native-gesture-handler';
+import { FlatList } from 'react-native-gesture-handler';
 import styles from './style';
 
 export default function TabOneScreen() {
+  const data = [
+    {
+      id: 1,
+      name: 'nguyen anh nhat',
+    },
+    {
+      id: 2,
+      name: 'nguyen trung phap'
+    },
+    {
+      id: 3,
+      name: 'nguyen thi tu binh'
+    },
+    {
+      id: 4,
+      name: 'nguyen thi tu binh'
+    },
+    {
+      id: 5,
+      name: 'nguyen thi tu binh'
+    },
+    {
+      id: 6,
+      name: 'nguyen thi tu binh'
+    }
+  ]
+  const [listCategory] = useState(data);
+
   return (
     <View>
       <Text style={styles.title}>Tab One</Text>
-        <ScrollView>
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-          <CategoryListItem />
-        </ScrollView>
+        <FlatList
+          data={listCategory}
+          renderItem={({ item }) => <CategoryListItem category={item} />}
+          keyExtractor={(item) => `${item.id}`}>
+        </FlatList>
+
 
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
